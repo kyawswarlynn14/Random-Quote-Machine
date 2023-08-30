f3d549b124e528aa3dcca2bfdad7dcec3cf16158
kyaw swar lynn <kyawswarlynn14@gmail.com>
2023-08-30 22:01:21 +0630

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'
 
-function App() {
+const App = () => {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  const fetchQuote = async () => {
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+    setQuote(data.content);
+    setAuthor(data.author);
+    changeBackgroundColor();
+  };
+
+  const handleNewQuote = () => {
+    fetchQuote();
+  };
+
+  const tweetQuote = () => {
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+      `"${quote}" - ${author}`
+    )}`;
+    window.open(tweetUrl, '_blank');
+  };
+
+  const changeBackgroundColor = () => {
+    let color = getRandomColor();
+    document.body.style.backgroundColor = color;
+    document.getElementById('quote-box').style.color = color;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div id="quote-box">
+      <div id="text">{quote}</div>
+      <div id="author">- {author}</div>
+      <button id="new-quote" onClick={handleNewQuote}>
+        New Quote
+      </button>
+      <a id="tweet-quote" href="#" onClick={tweetQuote}>
+        Tweet Quote
+      </a>
     </div>
+    <p id="name">by kyawswarlynn</p>
+    </>
   );
+};
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 export default App;
